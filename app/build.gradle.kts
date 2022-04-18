606d23e6b9559b9eceabb6c93b40b60a0c11b43c
(Bruce) Thien Nguyen <hoangthien0196@gmail.com>
2022-04-18 11:48:48 +0700

    Init project and setup dependencies

---
+++ app/build.gradle.kts
@@ -0,0 +1,141 @@
+plugins {
+    id ("com.android.application")
+    id ("org.jetbrains.kotlin.android")
+    id("kotlin-android")
+    id("kotlin-kapt")
+    id("kotlin-parcelize")
+    id("dagger.hilt.android.plugin")
+    id("androidx.navigation.safeargs.kotlin")
+}
+
+android {
+    compileSdk = 32
+
+    defaultConfig {
+        applicationId = "com.thiennguyen.survey"
+        minSdk = 21
+        targetSdk = 32
+        versionCode = 1
+        versionName = "1.0"
+        multiDexEnabled = true
+        vectorDrawables.useSupportLibrary = true
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = true
+            isDebuggable = false
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
+            )
+        }
+        getByName("debug") {
+            isMinifyEnabled = false
+            isTestCoverageEnabled = true
+        }
+    }
+
+    flavorDimensions.add("app")
+    productFlavors {
+        create("staging") {
+            applicationIdSuffix = ".staging"
+            versionNameSuffix = "-staging"
+            resValue("string", "app_name", "Survey Staging")
+        }
+        create("prod") {
+            resValue("string", "app_name", "Survey")
+        }
+    }
+
+    sourceSets["test"].resources {
+        srcDir("src/test/resources")
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
+
+    testOptions {
+        unitTests {
+            isIncludeAndroidResources = true
+            isReturnDefaultValues = true
+        }
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+    lint {
+        xmlOutput = file("build/reports/lint/lint-result.xml")
+        xmlReport = true
+    }
+}
+
+kapt {
+    correctErrorTypes = true
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+
+    // Kotlin
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.6.10")
+
+    // Android
+    implementation("com.google.android.material:material:1.5.0")
+    implementation("androidx.core:core-ktx:1.7.0")
+    implementation("androidx.fragment:fragment-ktx:1.4.1")
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+    implementation("androidx.appcompat:appcompat:1.4.1")
+    implementation("androidx.recyclerview:recyclerview:1.2.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.3")
+    implementation("androidx.multidex:multidex:2.0.1")
+
+    // Navigation
+    implementation("androidx.navigation:navigation-runtime-ktx:2.4.2")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.4.2")
+    implementation("androidx.navigation:navigation-ui-ktx:2.4.2")
+
+    // Lifecycle
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.4.1")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.4.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:2.4.1")
+    implementation("androidx.lifecycle:lifecycle-common-java8:2.4.1")
+
+    // Dagger
+    kapt("com.google.dagger:hilt-compiler:2.38.1")
+    implementation("com.google.dagger:hilt-android:2.38.1")
+
+    // Timber
+    implementation("com.jakewharton.timber:timber:5.0.1")
+
+    // Glide
+    kapt("com.github.bumptech.glide:compiler:4.8.0")
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+
+    // Rx
+    implementation("io.reactivex.rxjava3:rxandroid:3.0.0")
+    implementation("io.reactivex.rxjava3:rxjava:3.0.0")
+    implementation("io.reactivex.rxjava3:rxkotlin:3.0.0")
+
+    // Unit testing
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:4.0.0")
+    testImplementation("androidx.test:core:1.4.0")
+    testImplementation("androidx.test:runner:1.4.0")
+    testImplementation("androidx.test:rules:1.4.0")
+    testImplementation("androidx.test.ext:junit-ktx:1.1.3")
+    testImplementation("io.mockk:mockk:1.12.1")
+    testImplementation("com.google.dagger:hilt-android-testing:2.37")
+    kaptTest("com.google.dagger:hilt-android-compiler:2.37")
+    testAnnotationProcessor("com.google.dagger:hilt-android-compiler:2.37")
+}
---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.1.3")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.21")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.38.1")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.4.2")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        maven(url = "https://jitpack.io")
+        mavenCentral()
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
